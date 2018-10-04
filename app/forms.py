1088bc6a2a640b954db192089241348c0e287e38
Ramesh Balaji <post2rb@hotmail.com>
2018-10-04 17:43:08 -0400

    continue to work on db integration in webapp itself

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
+
 
 class RegistrationForm(FlaskForm):
 	first_name = StringField('First Name', validators=[DataRequired()])
 	last_name = StringField('Last Name', validators=[DataRequired()])
 	username = StringField('Username', validators=[DataRequired()])
 	email = EmailField('Email', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_validate', message='The passwords must match']])
+	password = PasswordField('Password', validators=[DataRequired(), EqualTo('password_validate', message='The passwords must match')])
 	password_validate = PasswordField('Password (type again to validate)', validators=[DataRequired()])
 	submit = SubmitField('Register')
 	
