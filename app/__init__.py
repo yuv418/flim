528f2a767e0d5bc7b11a7df324ddbbb304765894
Ramesh Balaji <post2rb@hotmail.com>
2018-10-11 22:33:36 -0400

    start css work

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app import config
 
 current_config = config.Config()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path=current_config.app_static_files_directory)
 app.config.from_object(current_config)
 
 login = LoginManager(app)
--- app/config.py
+++ app/config.py
@@ -11,11 +11,16 @@ class Config:
 	app_db_password = "letmeinmysql"
 	app_db_host = "localhost"
 	
-	app_post_topics_list = [("Help Needed", "Help Needed"), ("Idea", "Idea"), ("Not Urgent", "Not Urgent"), ("Urgent", "Urgent"), ("Windows", "Windows"), ("macOS", "macOS"), ("Linux", "Linux"),
+	app_post_topics_list = [("Announcement", "Announcement"), ("Help Needed", "Help Needed"), ("Idea", "Idea"), ("Not Urgent", "Not Urgent"), ("Urgent", "Urgent"), ("Windows", "Windows"), ("macOS", "macOS"), ("Linux", "Linux"),
 		("iOS", "iOS"), ("Android", "Android"), ("Windows Phone", "Windows Phone"), ("SIM Card Problem", "SIM Card Problem"), ("Phone", "Phone"), ("Desktop", "Desktop"), ("Laptop", "Laptop"),
 		("Broken", "Broken"), ("Data Recovery", "Data Recovery")] 
 		
 	
+	
+	
+	# DO NOT EDIT THE FOLLOWING!!!!!
+	
+	app_static_files_directory = "/static" # do not change, just makes things easier for us
 	
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'afioj89498ffj98hiahffuihfihwilafhliuifhUIHFIUHFIUHfieuahhu8yh4ih4foh'
 	SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}/{}".format(app_db_username, app_db_password, app_db_host, app_db_name)
