363202c929d4e9a063055e3617de4d059898b88f
Ramesh Balaji <post2rb@hotmail.com>
2018-10-17 13:35:43 -0400

    acp regulations work

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -2,21 +2,20 @@ from app import app
 from app.models import *
 
 from flask import render_template, abort, request
-from flask_login import current_user
+from flask_login import current_user, login_manager
 
 import sys 
 
 
 @app.before_request
 def restrict_admin_access():
+	
 	request_paths = request.path.split('/')
 	admin_group = Group.admin_group()
 	
-	if request_paths[0] == "admin":
+	if request_paths[1] == "admin":
 		if not admin_group in current_user.groups: # not an admin
-			response.status = 403
-			return response
-			
+			return 'Permission Denied', 403 
 	
 	
 
