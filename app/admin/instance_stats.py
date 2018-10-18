7ed23c5e865636f6c4c9032dced04bbe82ba1758
Ramesh Balaji <nonuser@localhost.localdomain>
2018-10-18 07:27:35 -0400

    work on instance stats

--- app/admin/instance_stats.py
+++ app/admin/instance_stats.py
@@ -12,3 +12,27 @@ class InstanceStats:
 		
 	def cpu_all_cores(self):
 		return psutil.cpu_count()
+	
+	def get_current_cpu_frequencies(self):
+		full_cpu_freqs = psutil.cpu_freq(percpu=True)
+		current_cpu_freqs = []
+		
+		
+		for scpufreq_listing in full_cpu_freqs:
+			current_cpu_freqs.append(spufreq_lsiting.current / 1000) #divide for GHz
+			
+		return current_cpu_freqs
+		
+	def total_mem(self):
+		return psutil.virtual_memory().total #TODO divide
+		
+	def available_mem(self):
+		return psutil.virtual_memory().available #TODO divide
+	
+	def total_swap_mem(self)
+		return psutil.swap_memory().total
+	
+	def available_swap_mem(self):
+		return psutil.swap_memory().free 
+	
+			
