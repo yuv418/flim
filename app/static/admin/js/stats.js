00e10aa32cd3a3635a72b7bea124eb21affc77d8
Ramesh Balaji <post2r@hotmail.com>
2018-10-19 14:47:01 -0400

    add free mem to frontend

--- app/static/admin/js/stats.js
+++ app/static/admin/js/stats.js
@@ -7,12 +7,21 @@ window.setInterval(function() {
 			
 			for (var i = 0; i < cpu_load_percent.length; i++){
 					var core_str = "Core " + (i+1) + ", Load: " + cpu_load_percent[i] + "%"
-					//console.log(core_str)
+					
 					core_p = $("<p class='core_str'></p>").text(core_str)
-					console.log(core_p)
 					$("#cpu_current_load_percent").append(core_p)
 			}
 				
+		})
+		
+		$.get("/admin/stats/avail_mem", function(data) {
+			
+			var avail_mem = Number(data).toFixed(2)
+			
+			var avail_mem_string = "Amount of free memory: " + avail_mem + "GB"
+			
+			$("#available_memory").html(avail_mem_string)
+			
 		})
 		
 }, 1000);
