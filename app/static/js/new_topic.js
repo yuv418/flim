85a19426914bb7a1315e0b9c39dbf15e132d86f4
Ramesh Balaji <post2r@hotmail.com>
2018-10-23 09:46:08 -0400

    add js for adding topics

--- app/static/js/new_topic.js
+++ app/static/js/new_topic.js
@@ -1,14 +1,30 @@
 $(function() {
 	$(".add_topic_button").click(function() {
-		console.log("add topic clicked")
 		
-		new_topic_name = $("<input></input")
+		if ($(".add_topic_button").html() === "X") {
+
+			$('.new_topic_submit').remove();
+			$('.new_topic_name').remove();
+			
+			$(".add_topic_button").html("+")
+				
+		}
+		else {
+			
+			
+			$(".add_topic_button").html("X")
+			
+			new_topic_name = $("<input class='new_topic_name'></input")
+			
+			new_topic_submit = $("<button type='button' class='new_topic_submit'></button")
+			new_topic_submit.html("Add")
+			
+			$(".new_topic_box").append(new_topic_name)
+			$(".new_topic_box").append(new_topic_submit)
 		
-		new_topic_submit = $("<button type='button' class='new_topic_submit'></button")
-		new_topic_submit.html("Add")
+			
+		}
 		
-		$(".new_topic_box").append(new_topic_name)
-		$(".new_topic_box").append(new_topic_submit)
 		
 	});
 	
