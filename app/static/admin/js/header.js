96108b164ac3f19b86c2904fd06d557f3d276b24
Ramesh Balaji <post2r@hotmail.com>
2018-10-24 07:30:30 -0400

    add more to header js where can get json object and use to generate menus

--- app/static/admin/js/header.js
+++ app/static/admin/js/header.js
@@ -1,20 +1,32 @@
-admin_user_btn_options = {
-    "links": {'preview': 'Nothing Here!', 'link' :  "#"}
+var admin_user_btn_options = {
+    "links": [{'preview': 'Nothing Here!', 'link' :  "#"},
+    
+				{'preview': 'Nothing here again!', 'link' : "#"}
+    
+    ]
+   
 };
 
 
 
-
 $(function() {
-	$("#navbtn").click(function() {
+	$(".header_navbtn").click(function() {
 	    // TODO convert the displaying of options here to get links and names from the (future) REST API
 	    div_links_box = $("<div></div>")
-	    if ($(this),attr('id') === "admin-user-btn") {
-		for (var i = 0; i < admin_user_btn_options.links; i++) {
-		    var preivew = admin_user_btn_options[i].preview
-		    var link = admin_user_btn_options[i].link
-		    
-		}
+	    
+	    if ($(this).attr('id') === "admin-user-btn") {
+			
+			console.log(admin_user_btn_options)
+			
+			for (var i = 0; i < admin_user_btn_options.links.length; i++) {
+				var link_preview = admin_user_btn_options.links[i].preview
+				var link = admin_user_btn_options.links[i].link
+
+
+				console.log("Preivew would be: " + link_preview)
+				console.log("Link would be: " + link)
+				
+			}
 
 	    }
 	    
