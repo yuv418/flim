4613c46263064a26cc25e77b56009395f67afcd9
Ramesh Balaji <post2r@hotmail.com>
2018-10-24 13:33:12 -0400

    links for users working

--- app/static/admin/js/header.js
+++ app/static/admin/js/header.js
@@ -11,35 +11,46 @@ var admin_user_btn_options = {
 
 $(function() {
 	$(".header_navbtn").click(function() {
-	    // TODO convert the displaying of options here to get links and names from the (future) REST API
-	    div_links_box = $("<div></div>")
-	    div_links_box.attr('class', 'link_box')
-	    
-	    if ($(this).attr('id') === "admin-user-btn") {
+		if ($(this).attr('data-menu-showing') === 'true') {
+			$('#' + $(this).attr('id') + '-link-box').remove()
 			
-			for (var i = 0; i < admin_user_btn_options.links.length; i++) {
-				var link_preview = admin_user_btn_options.links[i].preview
-				var link = admin_user_btn_options.links[i].link
-
-
-				link = $("<a></a>")
-				link.html(link_preview)
-				link.attr('href', link)
-				
-				div_links_box.append(link)
-				div_links_box.append("<br>")
-				
-				$(this).parent().append(div_links_box)
+			$(this).attr('data-menu-showing', 'false')
+		}
+		else {
+			// TODO convert the displaying of options here to get links and names from the (future) REST API
+			div_links_box = $("<div></div>")
+			div_links_box.attr('class', 'link_box')
+			div_links_box.attr('id', $(this).attr('id') + "-link-box")
 			
+			if ($(this).attr('id') === "admin-user-btn") {
+				
+				for (var i = 0; i < admin_user_btn_options.links.length; i++) {
+					var link_preview = admin_user_btn_options.links[i].preview
+					var link = admin_user_btn_options.links[i].link
 
-				console.log("Preivew would be: " + link_preview)
-				console.log("Link would be: " + link)
+
+					link = $("<a></a>")
+					link.html(link_preview)
+					link.attr('href', link)
+					
+					div_links_box.append(link)
+					div_links_box.append("<br>")
+					
+					$(this).parent().append(div_links_box)
 				
+
+					console.log("Preivew would be: " + link_preview)
+					console.log("Link would be: " + link)
+					
+					
+					
+				}
 				
-				
+				$(this).attr('data-menu-showing', 'true')
+
 			}
-
-	    }
+		}
+	    
 	    
 	});
 	
