186a84d56a4c54786a83a84b019571fcadf02413
Ramesh Balaji <post2r@hotmail.com>
2018-10-26 12:10:22 -0400

    work on issue

--- app/config_helper.py
+++ app/config_helper.py
@@ -5,6 +5,12 @@ current_config = config.Config()
 
 def get_full_post_topics_list():
 	config_topics_list = current_config.get_post_topics_list()
+	
+	print(f'Topic list {Topic.query.all()}')
+	
 	db_topics_list = [(topic.name, topic.name) for topic in Topic.query.all()]
+	
+	for topic in db_topics_list:
+		print(topic)
 			
 	return config_topics_list + db_topics_list
