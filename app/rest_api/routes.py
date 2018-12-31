e31d880ce22558a108134f130bf1fdb7fece0df1
Ramesh Balaji <post2rb@hotmail.com>
2018-12-31 15:38:15 -0500

    Update the rest_api directory structure in order for it to be easier to add update, create, and delete routes for the respective models

--- app/rest_api/routes.py
+++ app/rest_api/routes.py
@@ -3,10 +3,9 @@ from flask_restful import Resource
 from app import api
 
 from app.rest_api import test
-from app.rest_api import model_resources
-from app.rest_api import user_resources
-from app.rest_api import post_resources
-from app.rest_api import response_resources
-from app.rest_api import group_resources
+from app.rest_api.user_resources import user_retrieve
+from app.rest_api.post_resources import post_retrieve
+from app.rest_api.response_resources import response_retrieve
+from app.rest_api.group_resources import group_retrieve
 
 
