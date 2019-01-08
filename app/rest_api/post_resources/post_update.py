87519d0350931086357dad83319a9c411fd41d81
Ramesh Balaji <post2rb@hotmail.com>
2019-01-08 15:43:38 -0500

    Bootstrapify the user update form. This form will be discarded as well as many others later.

--- app/rest_api/post_resources/post_update.py
+++ app/rest_api/post_resources/post_update.py
@@ -5,6 +5,8 @@ from app.create.post import NewPost
 from flask import request
 from flask_restful import Resource
 
+import json
+
 class UpdatePostResource(Resource):
 	"""
 	A resource that allows you to delete a post from the REST API
@@ -13,7 +15,8 @@ class UpdatePostResource(Resource):
 	    (the following parameters are optional, this resource takes at least one:)
 	    title: string
 	    content: string
-	    topics: list (comma-separated values in string)
+	    topics: list (comma-separated values in string)	    
+	    topics_add: list (comma-separated values in string) (this value will append topics to the current list of topics)
 
 	Returns parameters:
 	    status: boolean
@@ -32,7 +35,7 @@ class UpdatePostResource(Resource):
 		new_title = request.values.get("title")
 		new_content = request.values.get("content")
 		new_topics_string = request.values.get("topics")
-		new_topics = new_topics_string.split(",")
+		
 		
 		# Step 3: Check if the post_id does not correspond to a post.
 
@@ -46,14 +49,15 @@ class UpdatePostResource(Resource):
 			
 		# Step 4: Check which values need to be updated and update the post with the new values.
 		
-		if new_topics != None:
-			post_to_update.topics = new_topics
+		if new_topics_string != None:
+			new_topics = new_topics_string.split(",")
+			post_to_update.topics = json.dumps(new_topics)
 
 		if new_title != None:
 			post_to_update.title = new_title
 
 		if new_content != None:
-			post_to_update.content = content
+			post_to_update.content = new_content
 
 		# Update the post with the new values
 
@@ -67,6 +71,4 @@ class UpdatePostResource(Resource):
 		return report
 
 api.add_resource(UpdatePostResource, "/api/post/update")
-			
-			
 		
