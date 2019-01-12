aafe588216b8f468eaf62703465301ee52ab5a69
Ramesh Balaji <post2rb@hotmail.com>
2019-01-12 12:06:04 -0500

    Comment the wsgi.py entry pointt

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,17 @@
 import sys
 import os
 
+# We first add the path for the main application so that we do not have import errors.
+
 sys.path.append(os.getcwd() + "/app")
 from app import app, config, db
 
+# We then retrieve the config so that we can start the web app on the right port and host.
+
 current_config = config.Config()
+
+# Then, we "create" the database. This will happen after an upgrade to flim. Normally, nothing will happen with db.create_all() unless there are database changes that need to be applied.
+
 db.create_all()
 
 if __name__ == "__main__":
