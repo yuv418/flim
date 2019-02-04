7ab39c6a45b4de3753e6b05d88aa9faacc844ddd
Ramesh Balaji <post2rb@hotmail.com>
2019-02-03 19:50:54 -0500

    Remove un-needed SQL schema folder

--- app/sql/schema.sql
+++
@@ -1,3 +0,0 @@
-use bug_reporter_db
-create table users (id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(128), last_name VARCHAR(128), email TEXT, password_hashed VARCHAR(256), username VARCHAR(32));
-create table post (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT, FOREIGN KEY(user_id) REFERENCES users(id), title VARCHAR(128), content TEXT, timestamp DATE)
