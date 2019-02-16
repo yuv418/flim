5c0a4b082a3059e60c4c96367446549124a942d7
cdknight <enterprisebalaji@hotmail.com>
2019-02-16 00:34:09 -0500

    Update API key add to use traditional function method. Furthermore, make this method more secure by requiring a password.

--- app/rest_api/api_resources/api_manage.py
+++ app/rest_api/api_resources/api_manage.py
@@ -1,18 +1,101 @@
 from app import app, api
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
 
 from app.rest_api.models import *
 from app.models import *
 
+# Authentication is NOT required for this.
 
-class APIKeyAddResource(Resource):
-	
-	# In order to add an API key, you MUST provide a user by their ID.
-	# Request parmeters: user_id (mandatory): user ID to add API key for
+@app.route("/api/key/add", methods=["PUT"])
+def api_key_add():
+    # In order to add an API key, you MUST provide a user by their ID.
+    # Request parmeters: username (mandatory): user ID to add API key for
+	# Request parameters: password (mandatory): password for user ID
 	# Request parmeters: description (optional) description for API key added to database
 	# Request returns "api_key" : string value of your API key. Keep this value VERY SAFE. Request also returns "status": true. (IF SUCCESSFUL)
 	# Request returns "msg" : string, "status" : false. (IF FAILED)
+
+	"""
+		This function is designed to generate and return a new API key for a user based on description and user ID provided.
+    """
+
+    # A blank report variable is generated.
+
+	report = {}
+
+	# STEP 1: Retrieve values from request and check if they are valid.
+
+	user_id = request.values.get("user_id")
+	password = request.values.get("password")
+	description = request.values.get("description")
+
+	if user_id == None:
+
+		# User ID is not valid! Return failing report.
+
+
+		report['status'] = False
+		report['msg'] = "No user ID was provided"
+
+		return jsonify(report), 403
+
+	if password == None:
+
+		# Passsword is not valid! Return a failing report.
+
+		report['status'] = False
+		report['msg'] = "No password was provided"
+
+		return jsonify(report), 403
+
+
+	# Check user ID and password to be valid
+
+	api_user = Users.query.filter_by(id=user_id).first()
+
+	if not api_user.check_password(password):
+
+		# The user is unauthorized to create an API key.
+
+		report['status'] = False
+		report['msg'] = "You are unauthorized to access this resource"
+
+		return jsonify(report), 403
+
+
+	# STEP 1b: Make sure the API user is validated with the credentials privided.
+
+
+
+	# STEP 2: Generate the API key.
+
+	# STEP 2a: Check if description is valid, we do not want to pass a None value to the database. If it isn't, we will give description a blank value.
+
+	if description == None:
+		description = "" 
+
+
+	# Step 2b: Actually create the API key
+
+	new_api_key = APIKey.create_api_key(api_user, description)
+
+	# Step 3: Generate report and send off.
+
+	report['status'] = True
+	report['api_key'] = new_api_key
+
+	return jsonify(report)
+
+
+
+
+
+
+
+class APIKeyAddResource(Resource):
+	
+	
 	
 	
 	def put(self):
@@ -130,7 +213,7 @@ class APIKeyDeleteResource(Resource):
 		
 		
 	
-api.add_resource(APIKeyAddResource, "/api/key/add")
+
 api.add_resource(APIKeyInfoResource, "/api/key/info")
 api.add_resource(APIKeyDeleteResource, "/api/key/delete")
 
