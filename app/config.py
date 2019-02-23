2058fa7d92232a6e2a8acc0eeb2ab8405d6b6333
cdknight <enterprisebalaji@hotmail.com>
2019-02-23 16:51:12 -0500

    Update config.py to work with config.json

--- app/config.py
+++ app/config.py
@@ -1,46 +1,52 @@
 import os
+import json
+
+# Open the config.json file in the 'app' directory
+
+config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
+with open(config_file_path) as config_file_object:
+	config_json = json.load(config_file_object)
 
 class Config:
-	
+
 	#************************ THE FOLLOWING SETTINGS **CANNOT BE** TO BE EDITABLE OUT OF THE CONFIG FILE *********************************************************
-	
-	
+
+
 	app_name = "Flim"
 	app_allow_anonymous_view_posts = True #TODO actually make this work
-	
-	app_db_name = os.getenv("FLIM_DB_NAME", default="flim") # no edits outside of config file.
-	app_db_username = os.getenv("FLIM_DB_USERNAME", "") # no edits outside of config file.
-	app_db_password = os.getenv("FLIM_DB_PASSWORD", "") # no edits outside of config file.
-	app_db_host = os.getenv("FLIM_DB_HOST", "") # no edits outside of config file.
-	app_db_provider = os.getenv("FLIM_DB_PROVIDER", "") # no edits outside of config file.
-	
+
+	app_db_name = config_json['database']['name'] or os.getenv("FLIM_DB_NAME", default="flim") # no edits outside of config file.
+	app_db_username = config_json['database']['username'] or os.getenv("FLIM_DB_USERNAME", "") # no edits outside of config file.
+	app_db_password = config_json['database']['password'] or os.getenv("FLIM_DB_PASSWORD", "") # no edits outside of config file.
+	app_db_host = config_json['database']['hostname'] or os.getenv("FLIM_DB_HOST", "") # no edits outside of config file.
+	app_db_provider = os.getenv("FLIM_DB_PROVIDER", "mysql") # no edits outside of config file.
+
 	app_message_max_length = 9990 # no edits outside of config file.
-		
-	
-	#************************ END SECTION *********************************************************
-	
-	
-	
-	#************************ THE FOLLOWING SETTINGS HAVE TO BE EDITABLE OUT OF THE CONFIG FILE *********************************************************
-	
-	
-	
-	file_app_post_topics_list = ["Announcement", "Help Needed", "Idea", "Not Urgent", "Urgent", "Windows", "macOS", "Linux", "iOS", "Android", "Windows Phone", 
-		"Ubuntu Touch", "KDE Plasma Mobile", "Question", "SIM Card Problem", "Cell Phone", "Desktop", "Laptop", "Broken", "Data Recovery"] # allow edits outside of config file.
-	
+
+
+#************************ END SECTION *********************************************************
+
+
+
+#************************ THE FOLLOWING SETTINGS HAVE TO BE EDITABLE OUT OF THE CONFIG FILE *********************************************************
+
+
+
+	file_app_post_topics_list = ["Announcement", "Help Needed", "Idea", "Not Urgent", "Urgent", "Windows", "macOS", "Linux", "iOS", "Android", "Windows Phone", "Ubuntu Touch", "KDE Plasma Mobile", "Question", "SIM Card Problem", "Cell Phone", "Desktop", "Laptop", "Broken", "Data Recovery"] # allow edits outside of config file.
+
 	file_app_allow_registration_tmp = os.getenv("FLIM_ALLOW_REGISTRATION") # allow edits outside of config file.
 	file_app_allow_registration = False
-	
+
 	if file_app_allow_registration_tmp == '1':
 		file_app_allow_registration = True
 	elif file_app_allow_registration_tmp == '0':
 		file_app_allow_registration = False
-	
-	
+
+
 	file_app_admin_group_id = 1 # allow edits outside of config file.
-	
+
 	#************************ END SECTION *********************************************************
-	
+
 	#************************ HOST INFORMATION SECTION ********************************************
 
 	app_host = os.getenv("FLIM_HOST", "127.0.0.1")
@@ -48,33 +54,32 @@ class Config:
 
 
 	#************************ END SECTION *********************************************************
-	
-	
-	
+
+
+
 	#*********************** DEFINE DB-EDITABLE VARS **********************************************
-	
+
 	app_allow_registration = file_app_allow_registration
 	app_post_topics_list = file_app_post_topics_list
 	app_admin_group_id = file_app_admin_group_id
-	
-	
+
+
 	#**********************************************************************************************
-	
-	
+
+
 	# DO NOT EDIT THE FOLLOWING!!!!!
-	
-	
-	
-	
-	
+
+
+
+
 	app_version = "TESTING"
 	app_static_files_directory = "/static" # do not change, just makes things easier for us
-	
+
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'afioj89498ffj98hiahffuihfihwilafhliuifhUIHFIUHFIUHfieuahhu8yh4ih4foh'
 	SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}/{}".format(app_db_provider, app_db_username, app_db_password, app_db_host, app_db_name)
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	
-	
+
+
 	def get_post_topics_list(self):
 		config_topics_list = [(topic, topic) for topic in self.app_post_topics_list]
 		return config_topics_list
