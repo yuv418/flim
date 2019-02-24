017799e230b7a3cd09bde3dd3b89933ad43d1eca
Ramesh Balaji <post2rb@hotmail.com>
2019-02-24 10:51:02 -0500

    No advanced filtering yet, but allow for Users to be searched for in the REST API by individual fields

--- app/rest_api/user_resources/user_retrieve.py
+++ app/rest_api/user_resources/user_retrieve.py
@@ -5,48 +5,32 @@ from flask_restful import Resource
 from app.models import *
 from app.schemas import *
 from app.rest_api.api_resources.api_decorators import *
-class UserByIDResource(Resource):
-	def get(self, user_id):
-		user = Users.query.filter_by(id=user_id).first()
-		return user.as_dict()
 
-class UserByUsernameResource(Resource):
-	def get(self, user_name):
-		user = Users.query.filter_by(username=user_name).first()
-		return user.as_dict()
+@app.route("/api/user/<field>/<value>")
+@api_require_auth
+def rest_user_by_field(field, value):
+	"""Filter a user by field and value"""
+	# Retrieve all models with the schema and filter
+	userschema = UserSchema(many=True)
+	all_users = Users.query.all()
+	users_list = userschema.dump(all_users).data
 
-class UserByFirstNameResource(Resource):
-	def get(self, first_name):
-		users = Users.query.filter_by(first_name=first_name).all()
-		return get_id_list(users)
+	# Filter the users
 
-class UserByLastNameResource(Resource): # Return a list of user IDs for each person with the same first name
-	def get(self, last_name):
-		users = Users.query.filter_by(last_name=last_name).all()
-		return get_id_list(users)
+	filtered_users = []
+	for user in users_list:
+		if str(user[field]) == value:
+			filtered_users.append(user)
 
+	# Return filtered users
 
-class UserByLastNameResource(Resource):  # Return a list of user IDs for each person with the same last name
-	def get(self, last_name):
-		users = Users.query.filter_by(last_name=last_name).all()
-		return get_id_list(users)
+	return jsonify(filtered_users)
 
-class UserByEmailResource(Resource):  # Return a list of user IDs for each person with the same e-mail 
-	def get(self, email):
-		users = Users.query.filter_by(email=email).all()
-		return get_id_list(users)
-
-@app.route("/api/user/all")
+@app.route("/api/user-all")
 @api_require_auth
 def rest_user_all():
 	userschema = UserSchema(many=True)
 	all_users = Users.query.all()
 	return userschema.jsonify(all_users)
 
-
-api.add_resource(UserByIDResource, "/api/user/by-id/<user_id>")
-api.add_resource(UserByUsernameResource, "/api/user/by-username/<user_name>")
-api.add_resource(UserByFirstNameResource, "/api/user/by-first_name/<first_name>")
-api.add_resource(UserByLastNameResource, "/api/user/by-last_name/<last_name>")
-api.add_resource(UserByEmailResource, "/api/user/by-email/<email>")
 
--- app/schemas.py
+++ app/schemas.py
@@ -4,7 +4,7 @@ from app import ma
 
 class UserSchema(ma.ModelSchema):
 	class Meta:
-		fields = ('about', 'id', 'first_name', 'last_name', 'email', 'groups', 'post', 'response')
+		fields = ('username', 'about', 'id', 'first_name', 'last_name', 'email', 'groups', 'post', 'response')
 		model = Users
 
 
