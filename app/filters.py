6bd1b6c665ebc194dfe5f11a83b7d817b6e23f8d
cdknight <enterprisebalaji@hotmail.com>
2019-03-02 23:06:02 -0500

    Allow markdown for user profile description and allow colors in filter.
    Also fix a small UX issue in the user profile page

--- app/filters.py
+++ app/filters.py
@@ -4,8 +4,12 @@ from markdown.extensions import Extension
 import bleach
 from bleach_whitelist import markdown_tags, markdown_attrs
 
+print(markdown_attrs)
+
 @app.template_filter('markdown')
 def markdown_filter(mdtext):
-	return bleach.clean(markdown.markdown(mdtext), markdown_tags, markdown_attrs)
+	allow_styles = markdown_attrs
+	allow_styles['*'] = 'style'
+	return bleach.clean(markdown.markdown(mdtext), markdown_tags, allow_styles, styles=['color'])
 
 
